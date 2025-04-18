46c455e903e3a9e11c64f1a7108b75a860491c2e
Shmuli Feld <shmulifeld@Shmulis-MacBook-Air.local>
2025-04-17 21:54:44 -0400

    misc commit

--- code/KNN/training_on_other_data/train_iris.py
+++ code/KNN/training_on_other_data/train_iris.py
@@ -3,7 +3,7 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
-from KNN import KNN
+from knn import KNN
 
 cmap = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
 
